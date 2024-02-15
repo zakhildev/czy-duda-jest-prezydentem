8524126a9c443ee8184f21f7b81e09d927c3c66b
zakhildev <piotr.blecharski02@gmail.com>
2024-02-15 17:39:27 +0100

    feat: Footer

--- app.vue
+++ app.vue
@@ -1,43 +1,62 @@
 <template>
-  <main>
-    <h1 class="que">Czy Andrzej Duda jest prezydentem?</h1>
-    <div class="ans">
-      <h2 v-if="isPresident" class="ans__yes">Tak!</h2>
-      <h2 v-else class="ans__no">Nie!</h2>
-    </div>
-    <div v-if="isPresident" class="countdown">
-      <p>Andrzej Duda będzie jeszcze prezydentem przez:</p>
-      <span class="years" v-if="duration.years() !== 0">
-        {{ `${duration.years()} rok ` }}
-      </span>
-      <span class="months" v-if="duration.months() !== 0">
-        {{ `${duration.months()} ${getWordForm(duration.months(), 'month')} ` }}
-      </span>
-      <span class="weeks" v-if="duration.weeks() !== 0">
-        {{ `${duration.weeks()} ${getWordForm(duration.weeks(), 'week')} ` }}
-      </span>
-      <span class="days" v-if="duration.days() !== 0">
-        {{ `${duration.days()} ${getWordForm(duration.days(), 'day')} ` }}
-      </span>
-      <span class="hours" v-if="duration.hours() !== 0">
-        {{ `${duration.hours()} ${getWordForm(duration.hours(), 'hour')} ` }}
-      </span>
-      <span class="minutes" v-if="duration.minutes() !== 0">
-        {{
-          `${duration.minutes()} ${getWordForm(duration.minutes(), 'minute')} `
-        }}
-      </span>
-      <span class="seconds" v-if="duration.seconds() !== 0">
-        {{
-          `${duration.seconds()} ${getWordForm(duration.seconds(), 'second')} `
-        }}
-      </span>
-    </div>
-    <div v-else class="countdown">
-      <p>Andrzej Duda nie jest już prezydentem!</p>
-      <p>Miłego dnia, smacznej kawusi, wiadomo kogo i wiadomo co. UwU</p>
-    </div>
-  </main>
+  <div class="container">
+    <main>
+      <h1 class="que">Czy Andrzej Duda jest prezydentem?</h1>
+      <div class="ans">
+        <h2 v-if="isPresident" class="ans__yes">Tak!</h2>
+        <h2 v-else class="ans__no">Nie!</h2>
+      </div>
+      <div v-if="isPresident" class="countdown">
+        <p>Andrzej Duda będzie jeszcze prezydentem przez:</p>
+        <span class="years" v-if="duration.years() !== 0">
+          {{ `${duration.years()} rok ` }}
+        </span>
+        <span class="months" v-if="duration.months() !== 0">
+          {{
+            `${duration.months()} ${getWordForm(duration.months(), 'month')} `
+          }}
+        </span>
+        <span class="weeks" v-if="duration.weeks() !== 0">
+          {{ `${duration.weeks()} ${getWordForm(duration.weeks(), 'week')} ` }}
+        </span>
+        <span class="days" v-if="duration.days() !== 0">
+          {{ `${duration.days()} ${getWordForm(duration.days(), 'day')} ` }}
+        </span>
+        <span class="hours" v-if="duration.hours() !== 0">
+          {{ `${duration.hours()} ${getWordForm(duration.hours(), 'hour')} ` }}
+        </span>
+        <span class="minutes" v-if="duration.minutes() !== 0">
+          {{
+            `${duration.minutes()} ${getWordForm(
+              duration.minutes(),
+              'minute'
+            )} `
+          }}
+        </span>
+        <span class="seconds" v-if="duration.seconds() !== 0">
+          {{
+            `${duration.seconds()} ${getWordForm(
+              duration.seconds(),
+              'second'
+            )} `
+          }}
+        </span>
+      </div>
+      <div v-else class="countdown">
+        <p>Andrzej Duda nie jest już prezydentem!</p>
+        <p>Miłego dnia, smacznej kawusi, wiadomo kogo i wiadomo co. UwU</p>
+      </div>
+    </main>
+    <footer>
+      Made by
+      <a href="https://github.com/zakhildev">Zakhil</a> using
+      <a href="https://nuxt.com/">Nuxt 3</a>.
+      <br />
+      <a href="https://github.com/zakhildev/czy-duda-jest-prezydentem/">
+        Github
+      </a>
+    </footer>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -61,12 +80,26 @@
 </script>
 
 <style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+    padding: 5px;
+    height: 100vh;
+  }
+
   main {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-flow: column;
     padding: 5px;
+    flex: 1;
+  }
+
+  footer {
+    text-align: center;
   }
 
   .que {
@@ -99,7 +132,20 @@
     font-size: 1.5rem;
   }
 
-  @media (max-width: 780px) {
+  footer a {
+    color: var(--link-blue);
+    text-decoration: none;
+  }
+
+  footer a:visited {
+    color: var(--link-blue);
+  }
+
+  footer a:hover {
+    text-decoration: underline;
+  }
+
+  @media (max-width: 800px) {
     .que {
       font-size: 2rem;
     }
